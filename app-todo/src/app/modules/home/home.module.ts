ef525e6a39ae6dce551796259adc383dcb7df15d
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-02-25 12:41:07 -0300

    Home

--- app-todo/src/app/modules/home/home.module.ts
+++ app-todo/src/app/modules/home/home.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//Componentes
 import { HeaderComponent } from './components/header/header.component';
 import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
 import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+//Page
 import { HomeComponent } from './pages/home/home.component';
 
 
